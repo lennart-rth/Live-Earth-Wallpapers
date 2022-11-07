feddec4d82978d80a617532aca082b7c6ad1609c
L-Roth <80266866+L-Roth@users.noreply.github.com>
2022-11-08 00:25:06 +0100

    update open souce guidlines

--- changeBackground.py
+++ changeBackground.py
@@ -1,39 +1,108 @@
 #!/usr/bin/env python3
+import argparse
 import datetime
 import os
-import argparse
-import sys
 import random
+import sys
 
-from full_disks import buildUrl,getImage
-from sentinel import fetchImage
-from utils import setBG, make_border
-from nasa_sdo import getSDOImage
+from full_disks import build_url, get_image
+from nasa_sdo import get_sdo_image
+from sentinel import fetch_image
+from utils import make_border, set_background
 
+sources = [
+    "goes-16",
+    "goes-17",
+    "goes-18",
+    "himawari",
+    "meteosat-9",
+    "meteosat-11",
+    "sentinel",
+    "sdo",
+]
+random_sources = [
+    "goes-16",
+    "goes-17",
+    "goes-18",
+    "himawari",
+    "meteosat-9",
+    "meteosat-11",
+    "sdo",
+]
 
-sources = ["goes-16", "goes-17", "goes-18", "himawari", "meteosat-9", "meteosat-11", "sentinel", "sdo"]
-random_sources = ["goes-16", "goes-17", "goes-18", "himawari", "meteosat-9", "meteosat-11", "sdo"]
 
 def parseArgs():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-z", "--zoomLevel", type=int, choices=[0, 1, 2, 3, 4], default=3,
-                            help="Change the ImageSize 0=678, 1=1356, 2=2712, 3=5424, 4=10848 (Meteosat does not support Level 4)")
-    parser.add_argument("-s", "--source", type=str, choices=sources,
-                            help="Select Satellite as a source. goes-16, goes-17, goes-18, himawari, meteosat-9, meteosat-11, sentinel, sdo (NASA Solar Dynamics Observatory)")
-    parser.add_argument("-m", "--colorMode", type=str, choices=["geocolor","natural_color","0171", "0171pfss", "0304", "0304pfss","HMIIC"],
-                            help="Select a color composite. geocolor and natural_color for goes, meteosat and himawari. The rest is only for SDO")
-    parser.add_argument("-o", "--outFile", type=str,
-                            help="Full path to a dir to save all loaded images. If not specified no images will be saved. Useful for Timelapse generation")
-    parser.add_argument("-p", "--bgProgram", type=str, choices=["feh","nitrogen","gsettings","osascript","apple_defaults","windows"],
-                            help="Select Programm to set the Background.")
-    parser.add_argument("-a", "--latitude", type=float, default=40.474114,
-                            help="Set the latitude of the Background image bounding box you want to set. Only for Sentinel as source.")
-    parser.add_argument("-b", "--longitude", type=float, default=8.876953,
-                            help="Set the longitude of the Background image bounding box you want to set. Only for Sentinel as source.")
-    parser.add_argument("-w", "--width",type=int,
-                        help="wanted width of the Wallpaper Image")
-    parser.add_argument("-he", "--height",type=int,
-                        help="wanted heigth of the Wallpaper Image")
+    parser.add_argument(
+        "-z",
+        "--zoomLevel",
+        type=int,
+        choices=[0, 1, 2, 3, 4],
+        default=3,
+        help="Change the ImageSize 0=678, 1=1356, 2=2712, 3=5424, 4=10848 (Meteosat does not support Level 4)",
+    )
+    parser.add_argument(
+        "-s",
+        "--source",
+        type=str,
+        choices=sources,
+        help="Select Satellite as a source. goes-16, goes-17, goes-18, himawari, meteosat-9, meteosat-11, sentinel, sdo (NASA Solar Dynamics Observatory)",
+    )
+    parser.add_argument(
+        "-m",
+        "--colorMode",
+        type=str,
+        choices=[
+            "geocolor",
+            "natural_color",
+            "0171",
+            "0171pfss",
+            "0304",
+            "0304pfss",
+            "HMIIC",
+        ],
+        help="Select a color composite. geocolor and natural_color for goes, meteosat and himawari. The rest is only for SDO",
+    )
+    parser.add_argument(
+        "-o",
+        "--outFile",
+        type=str,
+        help="Full path to a dir to save all loaded images. If not specified no images will be saved. Useful for Timelapse generation",
+    )
+    parser.add_argument(
+        "-p",
+        "--bgProgram",
+        type=str,
+        choices=[
+            "feh",
+            "nitrogen",
+            "gsettings",
+            "osascript",
+            "apple_defaults",
+            "windows",
+        ],
+        help="Select Programm to set the Background.",
+    )
+    parser.add_argument(
+        "-a",
+        "--latitude",
+        type=float,
+        default=40.474114,
+        help="Set the latitude of the Background image bounding box you want to set. Only for Sentinel as source.",
+    )
+    parser.add_argument(
+        "-b",
+        "--longitude",
+        type=float,
+        default=8.876953,
+        help="Set the longitude of the Background image bounding box you want to set. Only for Sentinel as source.",
+    )
+    parser.add_argument(
+        "-w", "--width", type=int, help="wanted width of the Wallpaper Image"
+    )
+    parser.add_argument(
+        "-he", "--height", type=int, help="wanted heigth of the Wallpaper Image"
+    )
 
     try:
         args = parser.parse_args()
@@ -42,36 +111,37 @@ def parseArgs():
         sys.exit(0)
     return args
 
+
 if __name__ == "__main__":
     args = parseArgs()
 
     if args.source is None:
         args.source = random.choice(random_sources)
 
-    if args.source != "sentinel" and args.source != "sdo":
-        base_url = buildUrl(args)
-        bg = getImage(args,base_url)
-
-    elif args.source == "sdo":
-        bg = getSDOImage(args)
+    if args.source == "sdo":
+        bg = get_sdo_image(args)
 
     elif args.source == "sentinel":
-        bg = fetchImage(args)
+        bg = fetch_image(args)
+
+    else:
+        base_url = build_url(args)
+        bg = get_image(args, base_url)
 
     if args.width is not None and args.height is not None:
-        bg=make_border(bg,args.width,args.height)
+        bg = make_border(bg, args.width, args.height)
     elif args.width is not None:
-        bg=make_border(bg,args.width,bg.size[1])
+        bg = make_border(bg, args.width, bg.size[1])
     elif args.height is not None:
-        bg=make_border(bg,bg.size[0],args.height)
+        bg = make_border(bg, bg.size[0], args.height)
 
-    logDate = datetime.datetime.now(datetime.timezone.utc).strftime("%d_%m_%Y_%H_%M")
+    log_date = datetime.datetime.now(datetime.timezone.utc).strftime("%d_%m_%Y_%H_%M")
     filename = f"{os.path.dirname(os.path.realpath(__file__))}/backgroundImage.png"
     bg.save(filename)
     print(f"Image saved to: {filename}")
 
     if args.outFile is not None:
-        imagePath = f"{args.outFile}/{logDate}.png"
-        bg.save(imagePath)
+        image_path = f"{args.outFile}/{log_date}.png"
+        bg.save(image_path)
 
-    setBG(args.bgProgram,filename)
+    set_background(args.bgProgram, filename)
--- full_disks.py
+++ full_disks.py
@@ -1,68 +1,81 @@
 import datetime
-from PIL import Image
+import json
 import sys
+import urllib.request
+
+from PIL import Image
+
 from utils import download
-import urllib.request, json
 
-satelliteData = {
-        #(minute,second,MaxZoomLevel)
-        "goes-16":(10,20,4),
-        "goes-17":(10,31,4),
-        "goes-18":(10,20,4),
-        "himawari":(10,0,4),
-        "meteosat-9":(15,0,3),
-        "meteosat-11":(15,0,3)}
+satellite_data = {
+    # (minute,second,MaxZoomLevel)
+    "goes-16": (10, 20, 4),
+    "goes-17": (10, 31, 4),
+    "goes-18": (10, 20, 4),
+    "himawari": (10, 0, 4),
+    "meteosat-9": (15, 0, 3),
+    "meteosat-11": (15, 0, 3),
+}
 
-def getTimeCode(sat):
+
+def get_time_code(sat):
     url = f"https://rammb-slider.cira.colostate.edu/data/json/{sat}/full_disk/natural_color/latest_times.json"
     f = urllib.request.urlopen(url)
     data = json.load(f)
     latest = data["timestamps_int"][0]
-    date = datetime.datetime.strptime(str(latest), '%Y%m%d%H%M%S').strftime("%Y/%m/%d")
+    date = datetime.datetime.strptime(str(latest), "%Y%m%d%H%M%S").strftime("%Y/%m/%d")
 
     return latest, date
 
-def calcTileCoordinates(zoomLevel):
-    #zoomlevel 0-3 or 0-4 (depending on the satellite)
+
+def calc_tile_coordinates(zoomLevel):
+    # zoomlevel 0-3 or 0-4 (depending on the satellite)
     t_n = 2**zoomLevel
-    row = range(0,t_n)
-    col = range(0,t_n)
+    row = range(0, t_n)
+    col = range(0, t_n)
     return list(row), list(col)
 
 
-def buildUrl(args):
-    if (args.source == "meteosat-9" or args.source == "meteorsat-10") and args.zoomLevel > 4:
+def build_url(args):
+    if (
+        args.source == "meteosat-9" or args.source == "meteorsat-10"
+    ) and args.zoomLevel > 4:
         sys.exit("Meteosat does not support Zoom Levels greater than 4.")
 
-    timeCode, date = getTimeCode(args.source) 
+    time_code, date = get_time_code(args.source)
 
     name = args.colorMode
     if name == None:
-        name = "natural_color"       #default color mode
+        name = "natural_color"  # default color mode
 
-    supportedArgs = ["geocolor","natural_color"]
-    if name not in supportedArgs:
-        raise ValueError("Wrong parameter for colorMode: Meteorsat and Goes only support 'natural_color' or 'geocolor' as colorMode!")
+    supported_args = ["geocolor", "natural_color"]
+    if name not in supported_args:
+        raise ValueError(
+            "Wrong parameter for colorMode: Meteorsat and Goes only support 'natural_color' or 'geocolor' as colorMode!"
+        )
 
-    base_url = f"https://rammb-slider.cira.colostate.edu/data/imagery/{date}/{args.source}---full_disk/{name}/{timeCode}/0{args.zoomLevel}"
+    base_url = f"https://rammb-slider.cira.colostate.edu/data/imagery/{date}/{args.source}---full_disk/{name}/{time_code}/0{args.zoomLevel}"
     return base_url
 
-def getImage(args,base_url):
-    row, col = calcTileCoordinates(args.zoomLevel)
-    bg = Image.new('RGB', (0,0))
+
+def get_image(args, base_url):
+    row, col = calc_tile_coordinates(args.zoomLevel)
+    bg = Image.new("RGB", (0, 0))
     for r in row:
         for c in col:
             url = base_url + f"/{str(r).zfill(3)}_{str(c).zfill(3)}.png"
             print(f"Downloading Image ({r},{c}).{url}")
             img = download(url)
 
-            new_bg = Image.new('RGB', (img.width*(max(col)+1), img.height*(max(row)+1)))
+            new_bg = Image.new(
+                "RGB", (img.width * (max(col) + 1), img.height * (max(row) + 1))
+            )
             new_bg.paste(bg, (0, 0))
-            new_bg.paste(img, (img.width*(c), (r)*img.height))
+            new_bg.paste(img, (img.width * (c), (r) * img.height))
 
             bg = new_bg
-    #zoom out a bit
-    wallpaper = Image.new('RGB', (int(bg.width*1.2),int(bg.height*1.2)))
-    wallpaper.paste(bg, (int(0+(bg.width*0.1)), int(0+(bg.height*0.1))))
+    # zoom out a bit
+    wallpaper = Image.new("RGB", (int(bg.width * 1.2), int(bg.height * 1.2)))
+    wallpaper.paste(bg, (int(0 + (bg.width * 0.1)), int(0 + (bg.height * 0.1))))
 
     return wallpaper
